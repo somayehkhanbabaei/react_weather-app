7db346e50fce8f06d6813e9fffcb3131c3518bdf
somayehkhanbabaei <51757189+somayehkhanbabaei@users.noreply.github.com>
2023-11-22 10:21:59 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Weather from './Weather';
+
+function App() {
+  return (
+    <div className='App'>
+      <Weather city='Utrecht' />
+      <p>
+        Create by Somayeh Khanbabaei{' '}
+        <a href='https://github.com/somayehkhanbabaei/weather_app'>
+          Open Source
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default App;
